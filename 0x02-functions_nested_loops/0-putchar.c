666f33c25d261036aca69ee16710b4edc50e94a6
Mukheee <muktark2020@gmail.com>
2022-04-12 13:26:59 +0000

    My commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *main - Entry point
  *
@@ -10,7 +9,7 @@ int main(void)
 _putchar('_');
 _putchar('p');
 _putchar('u');
-_putcahr('t');
+_putchar('t');
 _putchar('c');
 _putchar('h');
 _putchar('a');
